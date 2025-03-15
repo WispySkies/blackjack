b79d07edc3ba68452f2eb2d43d6033cafd545089
WispySkies <arpolixgames@gmail.com>
2025-03-14 20:02:29 -0400

    Initial Commit

---
+++ blackjack/blackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackjackApp.swift
+//  blackjack
+//
+//  Created by Declan McCue on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct blackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
